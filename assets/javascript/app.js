3e82fde0ef4383bdcc96ebc689f92897afcbf4c6
Timothy Cho <tcho9522@gmail.com>
2018-11-20 23:28:40 -0600

    some updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,68 @@ var config = {
 
 firebase.initializeApp(config);
 var database = firebase.database();
-database.ref().set({
-    number: 2,
+var p1Status;
+var p2Status;
+
+initializeStartScreen();
+
+function initializeStartScreen() {
+    $(".playerScreen").html("");    // This blanks out the screen. Takes everything out
+    $(".playerScreen").append("<input type='text' name='userName' id='p1Input' > <br>")
+}
+
+$('input').on('click', function () {
+    console.log($(this).attr('id'))
 })
-console.log(database);    
+database.ref().on('value', function (snap) {
+    p1Status = snap.child('player1').val();
+    p2Status = snap.child('player2').val();
+    if (p1Status === false) {
+        $("#p1").append("<button class='startButton' playerButton='p1'>Join Game</button>")
+    }
+    else {
+        $("#" + player).html("<button class ='playerChoice' id='rock'>Rock</button> <button class ='playerChoice' id='paper'>Paper</button> <button class ='playerChoice' id='scissor'>Scissor</button>")
+    }
+    if (p2Status === false) {
+        $("#p2").append("<button class='startButton' playerButton='p2'>Join Game</button>")
+    }
+    else {
+        $("#" + player).html("<button class ='playerChoice' id='rock'>Rock</button> <button class ='playerChoice' id='paper'>Paper</button> <button class ='playerChoice' id='scissor'>Scissor</button>")
+    }
+})
+database.ref().set({        //Initializing the player status
+    player1: false,
+    player2: false
+})
+
+$(".startButton").click(function () {
+    var player = $(this).attr('playerButton');
+    console.log(player);
+    if ($(this).attr('playerButton') === 'p1') {
+        database.ref().set({
+            player1: true,
+            player2: null
+        })
+    }
+    else {
+        database.ref().set({
+            player1: null,
+            player2: true
+        })
+    }
+})
+
+// $(".playerButton").click(function(){
+//     var player = $(this).attr('buttonID');
+
+
+//     $("#" + player).html("<button class ='playerChoice' id='rock'>Rock</button> <button class ='playerChoice' id='paper'>Paper</button> <button class ='playerChoice' id='scissor'>Scissor</button>");
+
+//     $(".playerChoice").on('click', function(){
+//         var playerMove = $(this).attr('id');
+//         console.log(playerMove);
+//         database.ref(player).on('', function(snap){
+
+//         })
+//     })
+// })
